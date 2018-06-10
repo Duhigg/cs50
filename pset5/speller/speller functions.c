228589402ac2df993ce134793332e22c0357e9d4
Nicklas Fianda <nicklasfianda@gmail.com>
2018-06-10 18:56:04 +0200

    fix apostropgies, next size

--- pset5/speller/speller functions.c
+++ pset5/speller/speller functions.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <cs50.h>
 
 
 #define LENGHT 45
@@ -23,7 +24,8 @@ node;
 
     node *root; // root node, do not alter
 
-void recursion(node*);
+bool rec_empty(struct node *);
+int rec_size(struct node *, int *);
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
@@ -36,25 +38,31 @@ bool check(const char *word)
     {
         if (isalpha(word[i]))
         {
-            int word_idx = toupper(word[i]) - 'A';
-
-                // go to corresponding element in sprout
-                if(trav->sprout[word_idx])
-                    trav = trav->sprout[word_idx];
+            int cor_idx = toupper(word[i]) - 'A';
+            // go to corresponding element in sprout
+            if(trav->sprout[cor_idx])
+                trav = trav->sprout[cor_idx];
+            else
+                return false;
         }
-        else // current character must be an apostrophie, located in the last index of sprout(27)
+        else // current character must be an apostrophie, located in the last index of sprout(26)
         {
-            int word_idx = 27;
-            if(trav->sprout[word_idx])
-                trav = trav->sprout[word_idx];
+            int cor_idx = 26;
+            if(trav->sprout[cor_idx])
+                trav = trav->sprout[cor_idx];
+            else
+                return false;
         }
     }
     // once at the end of input word, check if the words match
-        if (i == strlen(word) - 1)
+      //printf("i == %d\nstrlen(word) == %lu\n", i, strlen(word));
+        if (i == strlen(word))
         {
             if(trav->is_word == 1)
                 return true;
         }
+
+       // printf("is_word: %d\n", trav->is_word);
 
     return false;
 }
@@ -64,26 +72,27 @@ bool check(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
-    root = malloc(sizeof(node));
+    root = calloc(sizeof(node), 1);
     node *trav = root;
     // store number of words in dictionary
-    int words = 0;
+    unsigned int words = 0;
     char temp[LENGHT + 1];
     FILE *temp_dict = fopen(dictionary, "r");
     while(fgets(temp, LENGHT + 1, temp_dict))
         words++;
     rewind(temp_dict); // rewind file position indicator
 
+    //printf("(load) words: %d\n", words);
 
     int cor_idx; // variable for indexing characters in words and their corresponding index in sprout
-
+    int word_idx; // index of the current char of word in temp
     // for every dictionary word, iterate throught the TRIE
     for(int dict_idx = 0; dict_idx < words; dict_idx++)
     {
         // read a word from dict into a buffer of size LENGHT(45 CHARACTERS) + 1 (/0) - defined in main as 45
         fgets(temp, LENGHT + 1, temp_dict);
         // for each character in temp load into TRIE
-        for(int word_idx = 0; word_idx < strlen(temp); word_idx++)
+        for(word_idx = 0; word_idx < strlen(temp) - 1; word_idx++)
         {
                 // check for case
                 if(isalpha(temp[word_idx]))
@@ -93,7 +102,7 @@ bool load(const char *dictionary)
                     cor_idx = temp[word_idx] - (int) 'A';
 
                     /* trav is not null on first iteration for the word,
-                        so check for roots cor_idx instead to avoid problems in next statement.
+                    so check for root's cor_idx instead to avoid problems in next statement.
                     */
                     if(word_idx == 0)
                     {
@@ -103,22 +112,25 @@ bool load(const char *dictionary)
                             trav->sprout[cor_idx] = new_node;
                             trav = trav->sprout[cor_idx];
                         }
+                        else
+                            trav = root->sprout[cor_idx];
                     }
-
-                    // if NULL, calloc a new node, have sprout[cor_idx] point to it and enter the new_node
-                    if (trav->sprout[cor_idx] == NULL)
+                    else
                     {
-                        node *new_node = calloc(sizeof(node), 1);
-                        // go into that node
-                        trav->sprout[cor_idx] = new_node;
-                        trav = trav->sprout[cor_idx];
+                        // if NULL, calloc a new node, have sprout[cor_idx] point to it and enter the new_node
+                        if (trav->sprout[cor_idx] == NULL)
+                        {
+                            node *new_node = calloc(sizeof(node), 1);
+                            trav->sprout[cor_idx] = new_node;
+                            trav = trav->sprout[cor_idx];
+                        }
+                        else // if not NULL, point to existing new_node
+                            trav = trav->sprout[cor_idx];
                     }
-                    else // if not NULL, point to existing new_node
-                        trav = trav->sprout[cor_idx];
                 }
-                else // neither upper or lower, current character must be and apostrophie index n-1
+                else // temp[word_idx] is not a character therefore it must be and apostrophie, index n-1 in sprout = 26
                 {
-                    cor_idx = sizeof(trav->sprout) / sizeof(node);
+                    cor_idx = 26;
                     // check for null pointers
                     if(trav->sprout[cor_idx] == NULL)
                     {
@@ -138,61 +150,111 @@ bool load(const char *dictionary)
                 }
 
         }
+        // if at end of word, set is_word to true
+       // printf("word_idx: %d\nstrlen(temp) - 1: %lu\n", word_idx, strlen(temp) - 1);
+        if(word_idx == strlen(temp) - 1)
+        {
+            trav->is_word = 1;
+            trav = root;
+        }
         if(dict_idx == words - 1)
-        return true;
+        {
+         fclose(temp_dict);
+         return true;
+        }
     }
     return false;
 }
 
-// Returns number of words in dictionary if loaded else 0 if not yet loaded
-unsigned int size(void)
-{
-    // TODO
-    return 0;
-}
+    // Returns number of words in dictionary if loaded else 0 if not yet loaded
+    unsigned int size(void)
+    {
+        /* Use recursion(rec_size) to search for is_word == 1 and increase count++
+            if is_word == 0 continue until it == 1.
+        */
+        // count_sum to be returned to main, count for rec_sum().
+        int *count;
+        count = malloc(sizeof(int));
+        //count_sum = malloc(sizeof(int));
+        *count = 0;
+        if(!root)
+            return 0;
+        else
+        {
+            node *trav = root;
+            for (int i = 0; i < 27; i++)
+            {
+                /* if trav->sprout[i] is not a null value there's a index value,
+                go into that node and check, in rec_sze, if the path contains a word(is_word == 1)
+                 */
+                if(trav->sprout[i])
+                {
+                    trav = trav->sprout[i];
+                    if(trav->is_word == 1)
+                        (*count)++;
+                    rec_size(trav, count);
+                }
+            }
+        }
+        unsigned int n = *count;
+        free(count);
+        return n;
+    }
+
+    int rec_size(struct node *trav, int *count)
+    {
+        for (int i = 0; i < 27; i++)
+        {
+           if(trav->sprout[i])
+            {
+                trav = trav->sprout[i];
+                if(trav->is_word == 1)
+                {
+                    (*count)++;
+                    //printf("cur_char: %c\n", (char) (i + 'A'));
+                }
+                rec_size(trav, count);
+            }
+        }
+        /* I think I am returning the value stored in the address
+        that count is pointing to back to size(), is that so?
+        */
+        return *count;
+    }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
     // TODO
-    node* cursor;
+    node *cursor;
     node *trav = root; // point trav to root
-    unsigned int size_sprout = sizeof(trav->sprout) / sizeof(node); // sizeof sprout[]
+    int root_idx;
+    const int size_sprout = 27; // sizeof sprout[]
     // iterate through root->sprout and free bottom up with recursion
-    for(int root_idx = 0; root_idx < size_sprout; root_idx++)
+    for(root_idx = 0; root_idx < size_sprout; root_idx++)
     {
         if(trav->sprout[root_idx])
         {
             cursor = trav->sprout[root_idx];
-            recursion(**cursor);
+            if(!rec_empty(cursor))
+                return false;
         }
     }
-
-    if (root == NULL && trav == NULL)
-    {
-        //if(trav == NULL)
-            free(trav);
-        //if(root == NULL)
-            free(root);
-
-        return true;
-    }
-return false;
+    return true;
 }
 
-void recursion(node*) //¤ INCOMPLETE
+bool rec_empty(struct node *cursor)
 {
-    node *trav;
-    // sizeof sprout[] pointer array
-    unsigned int size_sprout = sizeof(trav->sprout) / sizeof(node);
-    // loop to the bottom of the trie and free from bottom up
-    for(int i = 0; i < size_sprout; i++)
+    // loop to the bottom of the trie if(!) and free from bottom up
+    for(int i = 0; i < 27; i++)
     {
         if(cursor->sprout[i])
         {
-            recursion(**cursor->sprout[i]);
-            trav->sprout[i] = NULL; // just in case
+            if(!rec_empty(cursor->sprout[i]))
+                return false;
+            cursor->sprout[i] = NULL; // just in case
         }
     }
     free(cursor);
+    return true;
 }
